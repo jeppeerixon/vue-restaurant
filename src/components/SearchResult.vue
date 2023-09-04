bf12186ef7223a0e14c6f457bdb58c052b5b5e29
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-09-04 11:18:36 +0200

    very basic search result and confirmation components

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="searchResult">
+      <h1>Step 2 - Select time</h1>
+      <ul>
+        <li>18:00 <button>select</button></li>
+        <li>20:00 <button>select</button></li>
+      </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,37 +1,24 @@
-<template>
-    <div class="booking">
-      <h1>Step 1 - Search Day</h1>
-      <form id="searchDate">
-        <label for="totalGuests">Number of guests</label>
-        <select name="guests" id="guests">
-          <option value="">?</option>
-          <option value=1>1</option>
-          <option value=2>2</option>
-          <option value=3>3</option>
-          <option value=4>4</option>
-          <option value=5>5</option>
-          <option value=6>6</option>
-        </select>
+<script setup lang="ts">
+  import SearchDate from '@/components/SearchDate.vue';
+  import SearchResult from '@/components/SearchResult.vue'
+  import SearchConfirm from '@/components/SearchConfirm.vue'
+</script>
 
-        <label for="start">Select Date:</label>
-        <input type="date" id="date" name="date" value="2023-09-25" min="2023-09-25" max="2023-12-31" />
-      </form>
-    </div>
+<template>
+  <div class="booking">
+    <SearchDate />
+    <SearchResult />
+    <SearchConfirm />
+  </div>
 </template>
   
-<style>
-  @media (min-width: 1024px) {
-    .booking {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-
-    #searchDate {
-      display: flex;
-      flex-direction: column;
-    }
+<style scoped>
+  .booking {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
   }
 </style>
