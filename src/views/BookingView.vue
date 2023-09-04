a187179b85c9618c21e4371afb0c471be2957f65
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-09-04 09:55:57 +0200

    :tada: first commit

---
+++ src/views/BookingView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="booking">
+      <h1>This is the booking view!</h1>
+    </div>
+</template>
+  
+<style>
+  @media (min-width: 1024px) {
+    .booking {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
