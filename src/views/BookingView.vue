64dd0dcb06325295c3e6824049eb36022b10d210
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-09-04 10:53:28 +0200

    simple search form input

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="booking">
-      <h1>This is the booking view!!</h1>
+      <h1>Step 1 - Search Day</h1>
+      <form id="searchDate">
+        <label for="totalGuests">Number of guests</label>
+        <select name="guests" id="guests">
+          <option value="">?</option>
+          <option value=1>1</option>
+          <option value=2>2</option>
+          <option value=3>3</option>
+          <option value=4>4</option>
+          <option value=5>5</option>
+          <option value=6>6</option>
+        </select>
+
+        <label for="start">Select Date:</label>
+        <input type="date" id="date" name="date" value="2023-09-25" min="2023-09-25" max="2023-12-31" />
+      </form>
     </div>
 </template>
   
@@ -9,7 +24,14 @@
     .booking {
       min-height: 100vh;
       display: flex;
+      flex-direction: column;
       align-items: center;
+      justify-content: center;
+    }
+
+    #searchDate {
+      display: flex;
+      flex-direction: column;
     }
   }
 </style>
