ef25fcec053e46a84bbbbbeeb355ec13b6fc943c
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-04 12:57:47 +0200

    changed currency to sek

--- src/components/RestaurantMenu.vue
+++ src/components/RestaurantMenu.vue
@@ -6,52 +6,52 @@
         <div class="dish">
             <h2>Spaghetti Carbonara</h2>
             <p>Spaghetti with creamy egg and bacon sauce</p>
-            <p class="price">$12.99</p>
+            <p class="price">129 kr</p>
         </div>
         <div class="dish">
             <h2>Margherita Pizza</h2>
             <p>Tomato sauce, mozzarella, basil</p>
-            <p class="price">$10.99</p>
+            <p class="price">110 kr</p>
         </div>
         <div class="dish">
             <h2>Lasagna</h2>
             <p>Layered pasta with ricotta and meat sauce</p>
-            <p class="price">$14.99</p>
+            <p class="price">149 kr</p>
         </div>
         <div class="dish">
             <h2>Chicken Alfredo</h2>
             <p>Grilled chicken with creamy Alfredo sauce</p>
-            <p class="price">$15.99</p>
+            <p class="price">159 kr</p>
         </div>
         <div class="dish">
             <h2>Ravioli with Marinara</h2>
             <p>Stuffed pasta with tomato sauce</p>
-            <p class="price">$11.99</p>
+            <p class="price">119 kr</p>
         </div>
         <div class="dish">
             <h2>Caprese Salad</h2>
             <p>Tomato, mozzarella, basil, balsamic glaze</p>
-            <p class="price">$8.99</p>
+            <p class="price">89 kr</p>
         </div>
         <div class="dish">
             <h2>Vegetable Risotto</h2>
             <p>Creamy rice with mixed vegetables</p>
-            <p class="price">$13.99</p>
+            <p class="price">139 kr</p>
         </div>
         <div class="dish">
             <h2>Tiramisu</h2>
             <p>Coffee-flavored dessert with mascarpone</p>
-            <p class="price">$6.99</p>
+            <p class="price">69 kr</p>
         </div>
         <div class="dish">
             <h2>Minestrone Soup</h2>
             <p>Traditional Italian vegetable soup</p>
-            <p class="price">$7.99</p>
+            <p class="price">79 kr</p>
         </div>
         <div class="dish">
             <h2>Calzone</h2>
             <p>Stuffed pizza turnover with assorted fillings</p>
-            <p class="price">$11.99</p>
+            <p class="price">119 kr</p>
         </div>
     </div>
 </template>
