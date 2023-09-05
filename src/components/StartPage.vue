bef2f8a023669e3126b55737fce14f235360eddb
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-05 20:57:27 +0200

    Added possibility to edit and delete bookings.

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <h1> La Italia</h1>
-        <p> Best Italian restaurant in town</p>
+    <div class="img-wrapper">
+        <img :src="pastaFork">
+        <div>
+            <h1> La Italia</h1>
+            <p> Best Italian restaurant in town</p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import pastaFork from "../assets/forkwithpasta.jpg"
 </script>
 
 <style scoped>
+    .img-wrapper {
+        display: flex;
+        flex-direction: row;
+        background-color: transparent;
+        justify-content: center;
+        align-items: center;
+
+    }
+    img {
+        max-width: 10rem;
+        border-radius: 50%;
+    }
     div {
         background-color: #42d9c8;
         margin: 2rem;
@@ -22,11 +37,19 @@
         padding: 2rem;
         border-top-left-radius: 10px;
         border-top-right-radius: 10px;
+        font-family: 'Caveat', cursive;
+        font-family: 'Playfair', serif;
+        font-style: italic;
+        font-size: 3rem;
     }
 
     p {
         color: #28464b;
         padding: 2rem;
+        font-family: 'Caveat', cursive;
+        font-family: 'Playfair', serif;
+        font-style: italic;
+        font-size: 1.5rem;
     }
 
 
