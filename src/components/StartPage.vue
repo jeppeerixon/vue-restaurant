debc69a39abe09410929a87866c806bec4d3026e
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-09-05 09:34:30 +0200

    :lipstick color changes

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -13,12 +13,15 @@
     div {
         background-color: #42d9c8;
         margin: 2rem;
+        border-radius: 10px;
         
     }
     h1 {
         background-color: #42d9c8;
         color: #28464b;
         padding: 2rem;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
     }
 
     p {
